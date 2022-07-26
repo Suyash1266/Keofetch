64c5a34b3f969151ada669dd3e02089c418341d4
Suyash Saxena <35092213+Suyash1266@users.noreply.github.com>
2022-07-26 20:39:51 +0530

    update install.sh
    - added checks for package managers.
    - added installation script for brew.

--- install.sh
+++ install.sh
@@ -2,15 +2,62 @@
 
 # check if neofetch is not installed
 if [ ! -x "$(command -v neofetch)" ]; then
-  # install neofetch
-  echo "neofetch is not installed, installing..."
-  # install with brew if on mac
-  if [ -x "$(command -v brew)" ]; then
-    brew install neofetch
-  else
-    # install with apt-get if on linux
-    apt install neofetch
-  fi
+    # install neofetch
+    echo "neofetch is not installed. This package requires it. Press enter to install it or any other key to exit."
+    var=$(read -r)
+    if [ "$var" == "" ]; then
+        echo "Installing neofetch..."
+        if [ "$(uname -s)" == "Linux" ]; then
+            # check which package manager is installed
+            if [ -x "$(command -v apt)" ]; then
+                # install neofetch using apt
+                sudo apt install neofetch
+            elif [ -x "$(command -v pacman)" ]; then
+                # install neofetch using pacman
+                sudo pacman -S neofetch
+            elif [ -x "$(command -v yum)" ]; then
+                # install neofetch using yum
+                sudo yum install neofetch
+            elif [ -x "$(command -v dnf)" ]; then
+                # install neofetch using dnf
+                sudo dnf install neofetch
+            elif [ -x "$(command -v zypper)" ]; then
+                # install neofetch using zypper
+                sudo zypper install neofetch
+            else
+                # error message
+                echo "Unable to find a suitable package manager. Please install neofetch manually."
+                exit 1
+            fi
+        elif [ "$(uname -s)" == "Darwin" ]; then
+            # check if brew is installed
+            if [ -x "$(command -v brew)" ]; then
+                # install neofetch using brew
+                brew install neofetch
+            else
+                # error message
+                echo "Unable to find a suitable package manager. Press Enter to install brew (a package manager for MacOS) or any other key to exit."
+                var=$(read -r)
+                if [ "$var" == "" ]; then
+                    # install brew
+                    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+                    # install neofetch using brew
+                    if [ -x "$(command -v brew)" ]; then
+                        brew install neofetch
+                    else
+                        # error message
+                        echo "Unable to install neofetch. Please install neofetch manually."
+                        exit 1
+                    fi
+                else
+                    # exit
+                    exit 1
+                fi
+            fi
+        fi
+    else
+        exit 1
+    fi
 fi
 
 # copy keofetch.sh to /usr/local/bin
@@ -19,4 +66,8 @@ cp keofetch.sh /usr/local/bin/keofetch
 # make keofetch executable
 chmod +x /usr/local/bin/keofetch
 
-echo "Successfully installed keofetch!"
+if (command -v keofetch); then
+    echo "Successfully installed keofetch!"
+else
+    echo "Unable to install keofetch!"
+fi
